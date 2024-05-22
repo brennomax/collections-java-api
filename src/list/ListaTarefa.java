2cd7d717e21e39666c80375537642379929f2452
Breno Max <brenomaxufmg@gmail.com>
2024-05-22 17:08:28 -0300

    Initial commit add: package list

---
+++ src/list/ListaTarefa.java
@@ -0,0 +1,12 @@
+package list;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+}
---
+++ src/list/Tarefa.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.lang;
+
+public class Tarefa {
+    private String tarefa;
+
+    public Tarefa(String tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public String getTarefa() {
+        return tarefa;
+    }
+}
